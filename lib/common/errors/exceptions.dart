d48be2242bf2570b4c37a889e58b1016795c431d
lechatnau <firnen76@gmail.com>
2023-09-23 16:44:14 +0300

    add series model

--- lib/common/errors/exceptions.dart
+++ lib/common/errors/exceptions.dart
@@ -2,3 +2,8 @@ class FileException implements Exception {
   final String cause;
   FileException(this.cause);
 }
+
+class ModelException implements Exception {
+  final String cause;
+  ModelException(this.cause);
+}
